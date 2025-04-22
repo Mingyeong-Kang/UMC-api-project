81da0a8ad7b241b1244d323370c4b4abf5d93028
Mingyeong-Kang <146615207+Mingyeong-Kang@users.noreply.github.com>
2025-04-22 19:22:54 +0900

    chore: remove chapter 04 code from main

--- sql/chapter04_create_users.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE my_api_project;
-USE my_api_project;
-
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50),
-    email VARCHAR(100),
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-);
